632f3a5e1d365901c23d0a7c7984fbcde225647e
erwinek <erwinek@o2.pl>
2019-12-02 20:44:38 +0100

    termostat M1

--- ReadDs.py
+++ ReadDs.py
@@ -1,8 +1,11 @@
 import sys 
 from time import sleep 
 import urllib2 
+import RPi.GPIO as GPIO
 
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
 
 tempfile1 = open("/sys/bus/w1/devices/10-000801d2ca71/w1_slave")
 thetext1 = tempfile1.read()
@@ -21,6 +24,7 @@ temperature2 = float(tempdata2[2:])
 temperatureTxt2 = temperature2 / 1000
 print (temperatureTxt2)
 
+#Kuchnia M1
 tempfile = open("/sys/bus/w1/devices/28-000005aa4abf/w1_slave")
 thetext1 = tempfile.read()
 tempfile.close()
@@ -28,6 +32,10 @@ tempdata1 = thetext1.split("\n")[1].split(" ")[9]
 temperature1 = float(tempdata1[2:])
 temperatureTxt3 = temperature1 / 1000
 print (temperatureTxt3)
+if temperatureTxt3 < 23:
+	GPIO.output(17, GPIO.HIGH)
+else:
+	GPIO.output(17, GPIO.LOW)
 
 tempfile = open("/sys/bus/w1/devices/28-01161da94dee/w1_slave")
 thetext1 = tempfile.read()
